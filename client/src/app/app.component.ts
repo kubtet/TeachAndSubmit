25136d9c27e0ee59194a40c838b72bbf7c17c71f
Jakub T <kubtet1@wp.pl>
2024-05-20 21:20:38 +0200

    Data fetched

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Teach&Submit';
+
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit() {
+    const users = await firstValueFrom(
+      this.http.get('https://localhost:5001/api/users')
+    );
+    console.log(users);
+  }
 }

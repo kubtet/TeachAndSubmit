27c70e84ba2ba2467675903bbb067a14585709b6
Jakub T <kubtet1@wp.pl>
2024-05-20 22:29:21 +0200

    Primeng installed

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
 import { UserService } from './services/user.service';
 import { User } from './models/user';
 

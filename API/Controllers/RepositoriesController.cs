9aa9abeb6452b453cbb5c672450519bc1904f41d
Jakub T <kubtet1@wp.pl>
2024-06-24 14:57:34 +0200

    New Repository endpoint added

--- API/Controllers/RepositoriesController.cs
+++ API/Controllers/RepositoriesController.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,13 +11,60 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Repository>>> GetRepositories()
         {
-            return await context.Repositories.ToListAsync();
+            return await context.Repositories
+                .Include(r => r.UserRepositories)
+                .Select(r => new Repository
+                {
+                    Id = r.Id,
+                    Subject = r.Subject,
+                    UserRepositories = r.UserRepositories.Select(ur => new UserRepository
+                    {
+                        UserId = ur.UserId,
+                    }).ToList()
+                })
+                .ToListAsync();
         }
 
         [HttpGet("{Id}")]
         public async Task<ActionResult<Repository>> GetRepositoryById(int id)
         {
-            return await context.Repositories.FindAsync(id);
+            var repository = await context.Repositories
+                .Include(r => r.UserRepositories)
+                .Where(r => r.Id == id)
+                .Select(r => new Repository
+                {
+                    Id = r.Id,
+                    Subject = r.Subject,
+                    UserRepositories = r.UserRepositories.Select(ur => new UserRepository
+                    {
+                        UserId = ur.UserId
+                    }).ToList()
+                })
+                .FirstOrDefaultAsync();
+
+            if (repository == null) return NotFound();
+
+            return repository;
+        }
+
+        [HttpGet("{Id}/users")]
+        public async Task<ActionResult<List<RepoUserDto>>> GetRepositoryUsersList(int Id)
+        {
+            var users = await context.UsersRepositories
+                .Where(ur => ur.RepositoryId == Id)
+                .Include(ur => ur.User)
+                .Select(ur => new RepoUserDto
+                {
+                    Id = ur.UserId,
+                    Name = ur.User.Name,
+                    Surname = ur.User.Surname,
+                    RoleId = ur.User.RoleId,
+                    RoleName = ur.User.Role.Name,
+                }).ToListAsync();
+
+            if (users == null) return NotFound();
+
+            return Ok(users);
         }
     }
 }
---
+++ API/DTOs/RepoUserDto.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class RepoUserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string RoleName { get; set; }
+        public int RoleId { get; set; }
+    }
+}
